89dfd94182efa8d453c09beb02c1911f3f7942dd
Your Name <conor.smith2@ucdconnect.ie>
2021-10-26 20:08:36 +0100

    Practical 4 solutions (source files in C)

---
+++ practical04/README.md
@@ -0,0 +1,3 @@
+The "trap_v2.c" script performs the first task in practical 4 where we are asked to put our radian values in an array which we then print
+
+The "trap_v3.c" script performs the second part of the task where we create a function that calculates the area using the trapezoidal rule.
---
+++ practical04/trap_v3.c
@@ -0,0 +1,55 @@
+//libraries for math funcitons and printf
+#include<stdio.h>
+#include<math.h>
+
+//declare functions before main block
+float degtorad(float degand);
+float traprule(int N, float TanArr[N+1]);
+
+int main(){
+
+	int N=12, i;
+	float TanArr[N+1],deg,rad;
+
+	//Calculate the values of f(xi) where i=0,1,...,12
+	// as array element ( xi: in  radians)
+
+	for(i=0;i<=N;i++){
+		deg=i*5.0;
+		rad=degtorad(deg);
+		TanArr[i]=tan(rad);
+		printf("TanArr[%d]=%f (f(x) at x=%d)\n",i,TanArr[i],i);
+	}
+	//Find the area by trapezoidal rule
+	float area;
+	area=traprule(N,TanArr);
+
+	//Approximate result
+	printf("\nTrapezoidal result:%f\n",area);
+	//Actual result (ln(2))
+	printf("Real Result: %f\n", log(2.0));
+return 0;
+}
+
+//function definitions after main block
+float degtorad(float degang){
+	return((M_PI*degang)/180.0);
+	}
+float traprule(int N, float TanArr[N+1]){
+		//define local variables for use in fumction
+		int i;
+		float area;
+
+		//implement area calculation as done previosuly (v2)
+	        area=TanArr[0]+TanArr[N];
+		for(i=1;i<N;i++){
+			area=area+2.0*TanArr[i];
+		}
+	
+		float mult_rad=degtorad((60.0-0.0)/(2.0*N));
+		area=mult_rad*area;
+
+		return area;
+		}
+
+
